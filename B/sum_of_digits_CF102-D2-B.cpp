ac221e96e2ef37ea7ccf776b3ba486c6da72d80c
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-05 23:12:46 -0400

    Fix some of digits

--- B/sum_of_digits_CF102-D2-B.cpp
+++ B/sum_of_digits_CF102-D2-B.cpp
@@ -3,27 +3,23 @@
 
 using namespace std;
 
-int E(string number, int size, int i, unsigned long long int sum, int times) {
-  if (i == size) {
-    number = to_string(sum);
-
-    size = number.size();
-    if (size == 1)
-      return sum == 0 ? times : times + 1;
-
-    return E(number, size, 0, 0, times + 1);
-  }
-
-  int a = (int)number[i] - 48;
-  return E(number, size, i + 1, sum + a, times);
-}
-
 int main() {
   string number;
   cin >> number;
 
-  int q = E(number, number.size(), 0, 0, 0);
-  cout << q;
+  int sum, count = 0, size = number.size();
+  while(size > 1) {
+    sum = 0;
+
+    for (int i = 0; i < size; i++)
+      sum += number[i] - '0';
+
+    number = to_string(sum);
+    size = number.size();
+    count++;
+  }
+
+  cout << count;
 
   return 0;
 }
