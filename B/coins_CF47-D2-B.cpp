8dcb0968676bfb687436f1924194e83e17ad5834
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-30 01:00:22 -0400

    [WIP] Add solution to the coins problem

---
+++ B/coins_CF47-D2-B.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// [B. Coins](http://codeforces.com/contest/47/problem/B)
+
+int main() {
+  // Request coins weight comparisons.
+  string w1, w2, w3;
+  cin >> w1;
+  cin >> w2;
+  cin >> w3;
+
+  string order = "";
+  if (w1[1] == '<') {
+    order += w1[0] + w1[2];
+  } else {
+    order += w1[2] + w1[0];
+  }
+
+  if (w2[1] == '<') {
+    if (w2[0] != order[0]) {
+      order = w2[0] + order;
+    } else {
+      order = order[0] + w2[2] + order[1];
+    }
+  } else {
+    if (w2[0] != order[0]) {
+      order = order[0] + w2[0] + order[1];
+    } else {
+      order = w2[2] + order;
+    }
+  }
+
+  int w31, w32;
+  for (int i = 0; i < 3; i++) {
+    char c = order[i];
+    
+    if (c == w3[0])
+      w31 = i;
+    if (c == w3[2])
+      w32 = i;
+  }
+
+  if ((w3[1] == '<' && w31 < w32) || (w3[1] == '>' && w31 > w32)) {
+    cout << order;
+  } else {
+    cout << "IMPOSIBLE";
+  }
+
+  return 0;
+}
