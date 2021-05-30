a8be505518d0cde69b59d2335c657bb06214cc03
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-30 15:02:19 -0400

    [WIP] Make code improves

--- B/effective_approach_CF227-D2-B.cpp
+++ B/effective_approach_CF227-D2-B.cpp
@@ -5,25 +5,6 @@
 using namespace std;
 
 // [B. Effective Approach](http://codeforces.com/contest/227/problem/B)
-int searchLinearly(int array[], int size, int target, bool fromFirst) {
-  int i = fromFirst ? 0 : size - 1;
-  int step = fromFirst ? 1 : -1;
-
-  while (i < size) {
-    int element = array[i];
-    if (element == target) {
-      break;
-    }
-
-    i += step;
-  }
-
-  if (fromFirst == true) {
-    return i + 1;
-  } else {
-    return size - i;
-  }
-}
 
 int main() {
   // INPUT:
@@ -34,15 +15,13 @@ int main() {
   //
   // OUTPUT:
   // 1: comparisons of Vasya's approach `a`, .. of Petya's approach `b`
-  // 
-  // SOLUTION:
+
   int n;
   cin >> n;
 
   int items[n];
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
     cin >> items[i];
-  }
 
   int m;
   cin >> m;
@@ -52,11 +31,14 @@ int main() {
   for (int i = 0; i < m; i++) {
     cin >> query;
 
-    int aSteps = searchLinearly(items, n, query, true);
-    int bSteps = searchLinearly(items, n, query, false);
-
-    a += aSteps;
-    b += bSteps;
+    for (int j = 0; j < n; j++) {
+      int element = items[j];
+      if (element == query) {
+        a += j + 1;
+        b += n - j;
+        break;
+      }
+    }
   }
 
   cout << a << " " << b;
