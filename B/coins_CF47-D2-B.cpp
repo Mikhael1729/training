ffdd59c2244595756bab6cfe56420407f94597dc
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-05-30 13:17:24 -0400

    Fix char concatenation problem

--- B/coins_CF47-D2-B.cpp
+++ B/coins_CF47-D2-B.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 // [B. Coins](http://codeforces.com/contest/47/problem/B)
 
+string str(char character) {
+  return string(1, character);
+}
+
 int main() {
   // Request coins weight comparisons.
   string w1, w2, w3;
@@ -14,29 +18,29 @@ int main() {
 
   string order = "";
   if (w1[1] == '<') {
-    order += w1[0] + w1[2];
+    order += str(w1[0]) + str(w1[2]);
   } else {
-    order += w1[2] + w1[0];
+    order += str(w1[2]) + str(w1[0]);
   }
 
   if (w2[1] == '<') {
     if (w2[0] != order[0]) {
-      order = w2[0] + order;
+      order = str(w2[0]) + order;
     } else {
-      order = order[0] + w2[2] + order[1];
+      order = str(order[0]) + str(w2[2]) + str(order[1]);
     }
   } else {
     if (w2[0] != order[0]) {
-      order = order[0] + w2[0] + order[1];
+      order = str(order[0]) + str(w2[2]) + str(order[1]);
     } else {
-      order = w2[2] + order;
+      order = str(w2[2]) + order;
     }
   }
 
   int w31, w32;
   for (int i = 0; i < 3; i++) {
     char c = order[i];
-    
+
     if (c == w3[0])
       w31 = i;
     if (c == w3[2])
@@ -46,7 +50,7 @@ int main() {
   if ((w3[1] == '<' && w31 < w32) || (w3[1] == '>' && w31 > w32)) {
     cout << order;
   } else {
-    cout << "IMPOSIBLE";
+    cout << "Imposible";
   }
 
   return 0;
